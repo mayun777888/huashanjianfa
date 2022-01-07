b88282fb37b061e82f50533c8a4a533c899f433b
mahuateng <645182623@qq.com>
2022-01-07 09:02:18 +0800

    Initial commit

---
+++ src/main/java/com/huashan/jianfa/Dugu9jian.java
@@ -0,0 +1,11 @@
+package com.huashan.jianfa;
+
+public class Dugu9jian {
+    public static void main(String[] args) {
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+        System.out.println("独孤九剑");
+    }
+}
